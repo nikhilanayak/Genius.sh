126dbed5db06cc91b2f53c19e1c4b272c77f11d5
Nikhil Nayak <nikhil.a.nayak@gmail.com>
2022-08-24 23:05:23 -0400

    Added GNU Parallel install

--- run.sh
+++ run.sh
@@ -2,11 +2,21 @@ install_node (){
   echo NodeJS not installed. Installing NodeJS..
 
   curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
-  sudo apt-get install -y nodejs
+  sudo apt update
+  sudo apt install -y nodejs
 }
 
 
+install_parallel (){
+  echo GNU Parallel not installed. Installing GNU Parallel..
+
+  sudo apt update
+  sudo apt install -y parallel
+}
+
 command -v node > /dev/null || install_node
+command -v parallel > /dev/null || install_parallel
+
 
 THREADS=1024
 BATCH_SIZE=4096
